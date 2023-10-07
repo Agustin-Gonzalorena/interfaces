6708684c69b050d356c0193951fa07b9f54935e1
Agustin-Gonzalorena <agonzalorena@hotmail.com>
2023-10-07 19:08:44 -0300

    terminando el home y yoshi tira wily

--- TP2/behaivors/game.js
+++ TP2/behaivors/game.js
@@ -1,53 +1,3 @@
-/* document.querySelector("#btnSlide1").addEventListener("click", () => {
-  document.querySelector("#slide1").classList.add("open");
-  document.querySelector("#slide2").classList.remove("open");
-  document.querySelector("#slide3").classList.remove("open");
-  document.querySelector("#slide4").classList.remove("open");
-  document.querySelector("#slide5").classList.remove("open");
-  document.querySelector("#slide6").classList.remove("open");
-});
-document.querySelector("#btnSlide2").addEventListener("click", () => {
-  document.querySelector("#slide2").classList.add("open");
-  document.querySelector("#slide1").classList.remove("open");
-  document.querySelector("#slide3").classList.remove("open");
-  document.querySelector("#slide4").classList.remove("open");
-  document.querySelector("#slide5").classList.remove("open");
-  document.querySelector("#slide6").classList.remove("open");
-});
-document.querySelector("#btnSlide3").addEventListener("click", () => {
-  document.querySelector("#slide3").classList.add("open");
-  document.querySelector("#slide1").classList.remove("open");
-  document.querySelector("#slide2").classList.remove("open");
-  document.querySelector("#slide4").classList.remove("open");
-  document.querySelector("#slide5").classList.remove("open");
-  document.querySelector("#slide6").classList.remove("open");
-});
-document.querySelector("#btnSlide4").addEventListener("click", () => {
-  document.querySelector("#slide4").classList.add("open");
-  document.querySelector("#slide1").classList.remove("open");
-  document.querySelector("#slide2").classList.remove("open");
-  document.querySelector("#slide3").classList.remove("open");
-  document.querySelector("#slide5").classList.remove("open");
-  document.querySelector("#slide6").classList.remove("open");
-});
-document.querySelector("#btnSlide5").addEventListener("click", () => {
-  document.querySelector("#slide5").classList.add("open");
-  document.querySelector("#slide1").classList.remove("open");
-  document.querySelector("#slide2").classList.remove("open");
-  document.querySelector("#slide3").classList.remove("open");
-  document.querySelector("#slide4").classList.remove("open");
-  document.querySelector("#slide6").classList.remove("open");
-});
-document.querySelector("#btnSlide6").addEventListener("click", () => {
-  document.querySelector("#slide6").classList.add("open");
-  document.querySelector("#slide1").classList.remove("open");
-  document.querySelector("#slide2").classList.remove("open");
-  document.querySelector("#slide3").classList.remove("open");
-  document.querySelector("#slide4").classList.remove("open");
-  document.querySelector("#slide5").classList.remove("open");
-});
- */
-// Función para cambiar la clase "open" en los elementos deslizantes
 function toggleSlide(slideId) {
   const slides = document.querySelectorAll(".slide");
   slides.forEach((slide) => {
@@ -58,28 +8,44 @@ function toggleSlide(slideId) {
     }
   });
 }
+function setActiveButton(buttonId) {
+  const buttons = document.querySelectorAll(".btnSlide");
+  buttons.forEach((button) => {
+    if (button.id === buttonId) {
+      button.classList.add("activeBtnImg");
+    } else {
+      button.classList.remove("activeBtnImg");
+    }
+  });
+}
 
 // Asignar la función a los eventos click de los botones
 document.querySelector("#btnSlide1").addEventListener("click", () => {
   toggleSlide("slide1");
+  setActiveButton("btnSlide1");
 });
 
 document.querySelector("#btnSlide2").addEventListener("click", () => {
   toggleSlide("slide2");
+  setActiveButton("btnSlide2");
 });
 
 document.querySelector("#btnSlide3").addEventListener("click", () => {
   toggleSlide("slide3");
+  setActiveButton("btnSlide3");
 });
 
 document.querySelector("#btnSlide4").addEventListener("click", () => {
   toggleSlide("slide4");
+  setActiveButton("btnSlide4");
 });
 
 document.querySelector("#btnSlide5").addEventListener("click", () => {
   toggleSlide("slide5");
+  setActiveButton("btnSlide5");
 });
 
 document.querySelector("#btnSlide6").addEventListener("click", () => {
   toggleSlide("slide6");
+  setActiveButton("btnSlide6");
 });
--- TP2/behaivors/home.js
+++ TP2/behaivors/home.js
@@ -1,4 +1,5 @@
 "use strict";
+import { apiGames } from "../utils/apiGames.js";
 
 function loading() {
   let count = document.querySelector(".count");
@@ -12,7 +13,7 @@ function loading() {
   async function updateProgressBar() {
     for (let i = 0; i <= 100; i++) {
       count.innerHTML = `<p>Cargando... %${i}</p>`;
-      await delay(5); // Esperar 50 ms antes de la siguiente iteración
+      await delay(50); // Esperar 50 ms antes de la siguiente iteración
     }
   }
 
@@ -22,3 +23,104 @@ function loading() {
   });
 }
 loading();
+
+function scrollCarousel(cant) {
+  const prevButtons = document.querySelectorAll(".carousel .prev");
+  const nextButtons = document.querySelectorAll(".carousel .next");
+
+  prevButtons.forEach((prevButton) => {
+    prevButton.addEventListener("click", function () {
+      const cardContainer = prevButton.nextElementSibling;
+      cardContainer.scrollLeft -= cant;
+      const cards = cardContainer.querySelectorAll(".card");
+      cards.forEach((card) => {
+        card.classList.add("moveLeft");
+      });
+      setTimeout(() => {
+        cards.forEach((card) => {
+          card.classList.remove("moveLeft");
+        });
+      }, 300);
+    });
+  });
+
+  nextButtons.forEach((nextButton) => {
+    nextButton.addEventListener("click", function () {
+      const cardContainer = nextButton.previousElementSibling;
+      cardContainer.scrollLeft += cant;
+      const cards = cardContainer.querySelectorAll(".card");
+      cards.forEach((card) => {
+        card.classList.add("moveRight");
+      });
+      setTimeout(() => {
+        cards.forEach((card) => {
+          card.classList.remove("moveRight");
+        });
+      }, 300);
+    });
+  });
+}
+scrollCarousel(400);
+
+document.querySelector("#adventure").innerHTML = apiGames
+  .filter((g) => {
+    return g.category_id == 3;
+  })
+  .map((g) => {
+    return `
+    <div class="card">
+    <div class="card-img" title="${g.name}">
+      <img
+      src="${g.img}"
+      alt="Imagen destacada"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${g.name}</h3>
+      </div>
+    </div>`;
+  })
+  .join("");
+
+document.querySelector("#rancing").innerHTML = apiGames
+  .filter((g) => {
+    return g.category_id == 4;
+  })
+  .map((g) => {
+    return `
+    <div class="card">
+    <div class="card-img" title="${g.name}">
+      <img
+      src="${g.img}"
+      alt="Imagen destacada"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${g.name}</h3>
+      </div>
+    </div>`;
+  })
+  .join("");
+
+document.querySelector("#offert").innerHTML = apiGames
+  .filter((g) => {
+    return g.discount == true;
+  })
+  .sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  })
+  .map((g) => {
+    return `
+    <div class="card">
+    <div class="card-img" title="${g.name}">
+      <img
+      src="${g.img}"
+      alt="Imagen destacada"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${g.name}</h3>
+      </div>
+    </div>`;
+  })
+  .join("");
